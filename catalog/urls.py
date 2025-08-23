0f1722e584af94ced5d3c9370036bf362fdaa4d2
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-08-23 14:17:24 +0300

    update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,7 @@ from catalog import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('', views.home, name='home_page'),
     path('contacts/', views.contacts, name='contacts')
 ]
 
