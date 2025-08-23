61ad863a5b0a059f58cf3fb7ec4c636cc33804fc
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-08-23 14:51:21 +0300

    update home page link

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from catalog import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    # path('home/', views.home, name='home'),
     path('', views.home, name='home_page'),
     path('contacts/', views.contacts, name='contacts')
 ]
