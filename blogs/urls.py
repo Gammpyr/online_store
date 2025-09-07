dfa1af91da7ab0b2a20906e5ea4cc511b77400c9
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-09-07 15:41:23 +0300

    update blogs files

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,6 +5,10 @@ from blogs import views
 app_name = 'blogs'
 
 urlpatterns = [
-
+    path('blogs/list/', views.BlogListView.as_view(), name='blogs_list'),
+    path('blogs/create/', views.BlogCreateView.as_view(), name='blog_create'),
+    path('blogs/detail/<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blogs/update/<int:pk>/', views.BlogUpdateView.as_view(), name='blog_update'),
+    path('blogs/delete/<int:pk>/', views.BlogDeleteView.as_view(), name='blog_delete'),
 ]
 
