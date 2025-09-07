00bb9903cee03c5e1116b3d9ea7ea9afedfc667e
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-09-07 15:40:26 +0300

    update admin.py

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blogs.models import Blog
+
+
+@admin.register(Blog)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_at', 'is_published', 'views_count')
+    list_filter = ('id', )
+    search_fields = ('title', 'content')
