d841430af8725a100456930c53fad952e8ecf520
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-09-27 01:13:45 +0300

    update users files

--- users/forms.py
+++ users/forms.py
@@ -3,16 +3,78 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
-    phone_number = forms.CharField(max_length=15, required=False, help_text='Необязательное поле. Введите ваш номер телефона.')
-    username = forms.CharField(max_length=50, required=True)
+    phone_number = forms.CharField(label='Номер телефона',
+                                   max_length=15,
+                                   required=False,
+                                   widget=forms.TextInput(
+                                       attrs={'class': 'form-control',
+                                              'autofocus': 'true',
+                                              }
+                                   ))
+    username = forms.CharField(label='Логин',
+                               max_length=50,
+                               help_text='Обязательное поле.',
+                               required=True,
+                               widget=forms.TextInput(
+                                   attrs={'class': 'form-control',
+                                          }
+                               ))
+    first_name = forms.CharField(label='Имя',
+                                 max_length=30,
+                                 required=False,
+                                 widget=forms.TextInput(
+                                     attrs={'class': 'form-control',
+                                            }), )
+    last_name = forms.CharField(label='Фамилия',
+                                max_length=30,
+                                required=False,
+                                widget=forms.TextInput(
+                                    attrs={'class': 'form-control',
+                                           }), )
+    country = forms.CharField(label='Страна',
+                              max_length=50,
+                              required=False,
+                              widget=forms.TextInput(
+                                  attrs={'class': 'form-control',
+                                         }), )
+    email = forms.EmailField(label='Почта',
+                             help_text='Обязательное поле.',
+                             widget=forms.EmailInput(
+                                 attrs={'class': 'form-control',
+                                        'autocomplete': 'username',
+                                        }), )
+    password1 = forms.CharField(label='Пароль',
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control',
+                                           'placeholder': 'Введите пароль',
+                                           'autocomplete': 'new-password',
+                                           }), )
+    password2 = forms.CharField(label='Повторите пароль',
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control',
+                                           'placeholder': 'Подтверждение пароля'
+                                           }), )
+    avatar = forms.ImageField(label='Аватар',
+                              required=False,
+                              widget=forms.FileInput(
+                                  attrs={'class': 'form-control'
+                                         }), )
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'username', 'first_name', 'last_name', 'phone_number', 'password1', 'password2')
+        fields = ('email', 'username', 'first_name', 'last_name', 'country', 'phone_number', 'password1', 'password2',
+                  'avatar')
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data.get('phone_number')
         if phone_number and not phone_number.isdigit():
             raise forms.ValidationError('Номер телефона должен состоять только из цифр')
         return phone_number
+
+    def clean_country(self):
+        country = self.cleaned_data.get('country')
+        if country and not country.isalpha():
+            raise forms.ValidationError('Страна должна состоять только из букв')
+        return country
--- users/views.py
+++ users/views.py
@@ -1,24 +1,25 @@
+from django.conf import settings
+from django.contrib.auth import login
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
-from django.core.mail import send_mail
-from django.contrib.auth import login
-from django.views.generic import CreateView
+
 from .forms import CustomUserCreationForm
+
 
 class RegisterView(FormView):
     template_name = 'users/register.html'
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('library:book_list')
+    success_url = reverse_lazy('catalog:product_list')
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        self.send_welcome_email(user.email)
+        self.send_welcome_email(user)
         return super().form_valid(form)
 
-    def send_welcome_email(self, user_email):
-        subject = 'Добро пожаловать в наш сервис'
-        message = 'Спасибо, что зарегистрировались в нашем сервисе!'
-        recipient_list = [user_email]
-        from_email = 'gamidpython@yandex.ru'
-        send_mail(subject, message, from_email, recipient_list)
+    def send_welcome_email(self, user):
+        subject = 'Добро пожаловать в наш магазин!'
+        message = f'{user.display_name.title()}, спасибо, что зарегистрировались в нашем сервисе!'
+        recipient_list = [user.email]
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
