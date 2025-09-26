5abb874d76b968e65bd33759f5f1eb1a39b2587a
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-09-27 01:13:54 +0300

    update config files

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
     'catalog',
     'blogs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
