fb48f2c007bb4404e076cf21144417e28903e937
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-09-27 01:12:43 +0300

    add users fields

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'phone_number', )
+    list_filter = ('first_name', 'last_name', 'email', 'country', )
+    search_fields = ('first_name', 'last_name', 'username', 'country', 'email', 'phone_number', )
